06eeede4eeef20f8c351e44793e056b937fd5d6e
sagan <sagan@syneforge.com>
2020-07-27 18:05:26 +0300

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import { radioPlayerInit } from "./radioPlayer.js";
+import { musicPlayerInit } from "./musicPlayer.js";
+import { videoPlayerInit } from "./videoPlayer.js";
+
+// variables
+const playerBtn = document.querySelectorAll('.player-btn');
+const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
+
+// function
+const deactivationPlayer = () => {
+    temp.style.display = '';
+    playerBtn.forEach(item => item.classList.remove('active'));
+    playerBlock.forEach(item => item.classList.remove('active'));
+}
+
+// event handler
+playerBtn.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+        deactivationPlayer();
+        btn.classList.add('active');
+        playerBlock[i].classList.add('active');
+    })
+})
+
+// function call
+radioPlayerInit();
+musicPlayerInit();
+videoPlayerInit();

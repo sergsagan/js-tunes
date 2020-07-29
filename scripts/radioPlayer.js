ff7152fbc17189609dc005bcbf34d320f12cdf4f
sagan <sagan@syneforge.com>
2020-07-29 17:07:49 +0300

    create radio player

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,3 +1,56 @@
 export const radioPlayerInit = () => {
-    console.log('Radio Init')
+    // variables
+    const radio = document.querySelector('.radio');
+    const radioPlayer = document.querySelector('.radio-player');
+    const radioNavigation = document.querySelector('.radio-navigation');
+    const radioCoverImg = document.querySelector('.radio-cover__img');
+    const radioHeaderBig = document.querySelector('.radio-header__big');
+    const radioItem = document.querySelectorAll('.radio-item');
+    const radioStop = document.querySelector('.radio-stop');
+
+    const audio = new Audio();
+    audio.type = 'audio/aac';
+    radioStop.disabled = true;
+
+    // function
+    const changeIconPlay = () => {
+         if (audio.paused) {
+             radio.classList.remove('play');
+             radioStop.classList.add('fa-play');
+             radioStop.classList.remove('fa-stop');
+         } else {
+             radio.classList.add('play');
+             radioStop.classList.remove('fa-play');
+             radioStop.classList.add('fa-stop');
+         }
+     };
+
+    const selectItem = elem => {
+         radioItem.forEach(item => item.classList.remove('select'));
+         elem.classList.add('select');
+     };
+
+    // event handler
+    radioNavigation.addEventListener('change', event => {
+         const target = event.target;
+         const  parent = target.closest('.radio-item');
+         selectItem(parent);
+
+         radioHeaderBig.textContent = parent.querySelector('.radio-name').textContent;
+         radioCoverImg.src = parent.querySelector('.radio-img').src;
+
+         radioStop.disabled = false;
+         audio.src = target.dataset.radioStation;
+         audio.play();
+         changeIconPlay();
+     });
+
+    radioStop.addEventListener('click', () => {
+          if (audio.paused) {
+              audio.play();
+          } else {
+              audio.pause();
+          }
+          changeIconPlay();
+      });
 }
